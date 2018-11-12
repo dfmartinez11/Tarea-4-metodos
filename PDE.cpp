37d7c7ce21159348828ac23e5666ac1c8a730b8f
Diegho Argüello <42391122+dfmartinez11@users.noreply.github.com>
2018-11-12 13:39:53 -0500

    Add files via upload

--- PDE.cpp
+++ PDE.cpp
@@ -3,39 +3,77 @@
 #include <stdio.h>
 using namespace std;
 
-#define Ymax 10000 //Esto es  100/0.01 = L/Dx
-#define Tmax 3000
-#define L 100
+#define Ymax 4
+#define Xmax 4 //Esto es  50/1 = L/Dx
+#define Tmax 1
+
+int i, j , k;
+double Cp = 820.0 / 1000.0 ; //  J/g*K
+double K = 1.62 *100.0 *100.0; //  cm*cm/s
+double p = 2.71; //  g/(cm*cm*cm)
+double nu = K/(Cp*p);
+
+//barilla de 10cm con 100C = 373.15K  
                         
-main()
+int main()
 {
-	double fut[Ymax], pres[Ymax] , pas[Ymax];
-	int i, j ;
-
+	cout << "nu =" << nu << endl;	
+	double pres[Xmax][Ymax] , fut[Xmax][Ymax];
 	double dt = 0.1;
-	double dx = 0.1;
-	double c = 2.0; //  esto es raiz(400/0.01)=raiz(T/ro)
-	double D = dx/dt;
-	//double n = c*c/(D*D);
-	double n = 1.0;
-
-	FILE *archivo;     
-	archivo = fopen("difus.dat","w");
-   
-	//condiciones iniciales
-	for(i=0; i<Ymax; i++){
-		pas[i]=0.0; 
-		if (i <= (0.8*L))	pres[i]=1.25*(-i/L);
+	double dx = 1.0; //tomamos cambios dx, dy como iguales
 		
-		if (i > (0.8*L))	pres[i]=(5-5*(-i))/L;
+	FILE *archivo;     
+	archivo = fopen("difusion.dat","w");
+   
+	for(j=0; j < Ymax; j++){
+		for(i=0; i < Xmax; i++){
+			pres[i][j]=293.15; 
+		}
+	}
+	//condiciones de borde con barilla
+	/*
+	for(j=0; 2 <= j <=3; j++){
+		for(i=0; 2 <= i <= 3; i++){
+			pres[i][j]=373.15; 
+		}
+	}
+	**/
+	
+	//condiciones de borde 10C
+	for(i=0; i < 4; i++){
+		pres[0][i]=283.15;
+		pres[i][0]=283.15;
+		pres[i][3]=283.15;
+		pres[3][i]=283.15;
 	}
 
-	pres[0]=0.0;        // bordes quietos
-	pres[Ymax] =0.0;    // bordes quietos
-      
+	
+	for(k=0 ; k<Tmax ; k++){ 
+		//cout << "  " << k*dt<< endl;
+		for(i=1 ; k<Xmax ; i++){
+			for(j=1 ; k<Ymax ; j++){
+				fut[i][j] = pres[i][j] + (dt*10/(dx*dx))*(-4*pres[i][j] + pres[i+1][j] + pres[i-1][j] + pres[i][j+1] + pres[i][j-1]);
+				fprintf(archivo, "%f %f %f %f", i*dx , j*dx , k*dt , pres[i][j]);
+				cout <<  i*dx << " " << j*dx <<" " << k*dt <<" " << pres[i][j] << endl;
+				pres[i][j] = fut[i][j];
+				
+			}
+			fprintf(archivo, "\n");	
+		}
+		fprintf(archivo, "\n");	
+		/*for(i=1 ; k<Xmax ; i++){
+			for(j=1 ; k<Ymax ; j++){
+				fprintf(archivo, "%f %f %f", i*dx , j*dx , k*dt );
+				pres[i][j] = fut[i][j];
+			}
+		}**/		
+	}
+	cout << "se completo el procedimiento" << endl;
+
+/*
 	for(j=0; j<Tmax; j++) //3000*0.001 =3segundos
 	{	   
-		for(i=1; i<(Ymax-1); i++)                 
+		for(i=1; i<Ymax; i++)                 
       		{
 
       			fut[i] = (2*pres[i])-pas[i] +0.5*(pres[i+1] + pres[i-1] - (2*pres[i]));
@@ -51,7 +89,8 @@ main()
         	//if(j%200 == 0) fprintf(output, "\n");
 	}
 
-	printf("---los datos estan en difus.dat\n");
+	printf("---los datos estan en difusion.dat\n");
 	fclose(archivo);
-
+**/
+	return 0;
 }
--- Plots_hw4.py
+++ Plots_hw4.py
@@ -2,43 +2,68 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = plt.figure()
+#a = plt.figure()
 lenn=10000
 
 plt.figure()
 #--------------------------------------------------------------
 #datos = np.loadtxt("difus.dat")
-posicion = np.loadtxt("posicion.dat")
-posicion10 = np.loadtxt("10_posicion.dat")
-posicion20 = np.loadtxt("20_posicion.dat")
-posicion30 = np.loadtxt("30_posicion.dat")
-posicion50 = np.loadtxt("50_posicion.dat")
-posicion60 = np.loadtxt("60_posicion.dat")
-posicion70 = np.loadtxt("70_posicion.dat")
+posicion = np.loadtxt( "posicion.dat" )
+posicion10 = np.loadtxt( "10_posicion.dat" )
+posicion20 = np.loadtxt( "20_posicion.dat" )
+posicion30 = np.loadtxt( "30_posicion.dat" )
+posicion50 = np.loadtxt( "50_posicion.dat" )
+posicion60 = np.loadtxt( "60_posicion.dat" )
+posicion70 = np.loadtxt( "70_posicion.dat" )
 
-plt.plot(posicion.transpose()[0],posicion.transpose()[1])
-plt.plot(posicion10.transpose()[0],posicion10.transpose()[1])
-plt.plot(posicion20.transpose()[0],posicion20.transpose()[1])
-plt.plot(posicion30.transpose()[0],posicion30.transpose()[1])
-plt.plot(posicion50.transpose()[0],posicion50.transpose()[1])
-plt.plot(posicion60.transpose()[0],posicion60.transpose()[1])
-plt.plot(posicion70.transpose()[0],posicion70.transpose()[1])
+
+plt.plot( posicion10.transpose()[0] , posicion10.transpose()[1] , label="10" )
+plt.plot( posicion20.transpose()[0] , posicion20.transpose()[1] , label="20" )
+plt.plot( posicion30.transpose()[0] , posicion30.transpose()[1] , label="30" )
+plt.plot( posicion.transpose()[0] ,posicion.transpose()[1] , label="40--" )
+plt.plot( posicion50.transpose()[0] , posicion50.transpose()[1] , label="50" )
+plt.plot( posicion60.transpose()[0] , posicion60.transpose()[1] , label="60" )
+plt.plot( posicion70.transpose()[0] , posicion70.transpose()[1] , label="70" )
+
+plt.legend()
 plt.title("posicion, Y vs X para diferentes angulos")
 plt.savefig("trayectoria.pdf")
+#plt.show()
 
-plt.show()
 
 velocidad = np.loadtxt("velocidadXY.dat")
-plt.plot(velocidad.transpose()[0],velocidad.transpose()[1])
-plt.title("velocidad_X vs velocidad_Y")
-plt.savefig("velX vs velY.pdf")
-plt.show()
-
 Mv = np.loadtxt("MagnitudVelocidad.dat")
-plt.plot(Mv.transpose()[0],Mv.transpose()[1])
-plt.title("magnitud velocidad Vs tiempo")
-plt.savefig("magnitud velocidad.pdf")
-plt.show()
+
+#---Con esto garantizo que no aparezcan puntos de ceros como en velocidad y Mv
+
+n=0
+for i in range(1, velocidad.shape[0]):
+	if (velocidad[i].all() != 0.0):
+		n += 1
+
+velocidadd = np.ones((n+1,2))
+for i in range(n+1):
+	velocidadd[i]=velocidad[i]
+
+
+k=0
+for i in range(1, Mv.shape[0]):
+	if (velocidad[i].all() != 0.0):
+		k += 1
+
+Mvv = np.ones((k+1,2))
+for i in range(k+1):
+	Mvv[i]=Mv[i]
+
+plt.subplot(1,2,1)
+plt.plot( velocidadd.transpose()[0] , velocidadd.transpose()[1] , label="Vy vs Vx" , c="b")
+plt.legend()
+plt.subplot(1,2,2)
+plt.plot( Mvv.transpose()[0],Mvv.transpose()[1] , label="|V| vs tiempo" , c="r")
+plt.title("componentes y magnitud de V con angulo=40    ")
+plt.savefig("Velocidad_40.pdf")
+plt.legend()
+#plt.show()
 
 
 
