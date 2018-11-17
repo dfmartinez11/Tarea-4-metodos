941194118112851af93aefcccf8d5f9595faae24
Diegho Argüello <42391122+dfmartinez11@users.noreply.github.com>
2018-11-17 13:17:06 -0500

    Add files via upload

--- PDE.cpp
+++ PDE.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define Xmax 50 //Esto es  50/1 = L/Dx
 #define Tmax 10
 
-int i, j , k;
+int i, j , k, l;
 double Cp = 820.0 / 1000.0 /273.15 ; //  J/g*C
 double K = 1.62 *100.0 *100.0; //  cm*cm/s
 double p = 2.71; //  g/(cm*cm*cm)
@@ -31,6 +31,8 @@ int main()
 			pres[i][j]=0.0; 
 		}
 	}
+
+
 		
 			
 	for(k=0 ; k<Tmax ; k++){ 
@@ -41,18 +43,17 @@ int main()
 				pres[i][j]=100.0; 
 			}
 		}**/	
-		pres[23][23]=100.0;			//inicializo directamente debido a un SEGMENTATION FAULT
+		pres[23][23]=100.0;	//inicializo directamente debido a un SEGMENTATION FAULT
 		pres[23][24]=100.0;
 		pres[23][25]=100.0;
 		pres[23][26]=100.0;
-		pres[24][23]=100.0;
 		pres[24][23]=100.0;
 		pres[24][24]=100.0;
 		pres[24][25]=100.0;
 		pres[24][26]=100.0;
 		pres[25][23]=100.0;
 		pres[25][24]=100.0;
-		pres[25][24]=100.0;
+		pres[25][25]=100.0;
 		pres[25][26]=100.0;
 		pres[26][24]=100.0;
 		pres[26][23]=100.0;
@@ -60,20 +61,47 @@ int main()
 		pres[26][26]=100.0;
 
 		//condiciones de borde exterior 10C
-		for(i=0; i < Xmax; i++){
-			pres[0][i]=10.0;
-			pres[49][i]=10.0;
-			pres[i][0]=10.0;
-			pres[i][49]=10.0;
+		for(l=0; l < Xmax; l++){
+			pres[0][l]=10.0;
+			pres[49][l]=10.0;
+			pres[l][0]=10.0;
+			pres[l][49]=10.0;
 		}
 
 		for(i=1 ; i<Xmax ; i++){
 
 			for(j=1 ; j<Ymax ; j++){
-				fut[i][j] = pres[i][j] + (dt*nu/(dx*dx))*(-4*pres[i][j] + pres[i+1][j] + pres[i-1][j] + pres[i][j+1] + pres[i][j-1]);
-				//fprintf(archivo, "%f %f %f %f \n", i*dx , j*dx , k*dt , pres[i][j]);
-				// cout <<  i*dx << " " << j*dx <<" " << k*dt <<" " << pres[i][j] << endl;
-				//pres[i][j] = fut[i][j];	
+
+				// (dt*nu/(dx*dx)) =1
+
+				fut[i][j] = pres[i][j] + 1*(-4*pres[i][j] + pres[i+1][j] + pres[i-1][j] + pres[i][j+1] + pres[i][j-1]);
+				/*fut[23][23]=100.0;// RE - inicializo directamente 
+				fut[23][24]=100.0;
+				fut[23][25]=100.0;
+				fut[23][26]=100.0;
+				fut[24][23]=100.0;
+				fut[24][24]=100.0;
+				fut[24][25]=100.0;
+				fut[24][26]=100.0;
+				fut[25][23]=100.0;
+				fut[25][24]=100.0;
+				fut[25][25]=100.0;
+				fut[25][26]=100.0;
+				fut[26][24]=100.0;
+				fut[26][23]=100.0;
+				fut[26][25]=100.0;
+				fut[26][26]=100.0;
+
+				for(l=0; l < Xmax; l++){
+					fut[0][l]=10.0;
+					fut[49][l]=10.0;
+					fut[l][0]=10.0;
+					fut[l][49]=10.0;
+				}
+
+				fprintf(archivo, "%f %f %f %f \n", i*dx , j*dx , k*dt , pres[i-1][j]);
+				//cout <<  i*dx << " " << j*dx <<" " << k*dt <<" " << pres[i][j] << endl;
+				pres[i-1][j] = fut[i-1][j];    **/	
 							
 			}
 					
@@ -84,18 +112,27 @@ int main()
 				fut[23][25]=100.0;
 				fut[23][26]=100.0;
 				fut[24][23]=100.0;
-				fut[24][23]=100.0;
 				fut[24][24]=100.0;
 				fut[24][25]=100.0;
 				fut[24][26]=100.0;
 				fut[25][23]=100.0;
 				fut[25][24]=100.0;
-				fut[25][24]=100.0;
+				fut[25][25]=100.0;
 				fut[25][26]=100.0;
 				fut[26][24]=100.0;
 				fut[26][23]=100.0;
 				fut[26][25]=100.0;
 				fut[26][26]=100.0;
+
+				for(l=0; l < Xmax; l++){
+					fut[0][l]=10.0;
+					fut[49][l]=10.0;
+					fut[l][0]=10.0;
+					fut[l][49]=10.0;
+				}
+	
+				if(fut[i-1][j] > 100.0)	fut[i-1][j] = 100.0;  //condicion de equilibrio hasta 100C
+				if(fut[i-1][j] < 0.0)	fut[i-1][j] = 0.0;  //condicion de minimo de temp. en 0.0C
 
 				fprintf(archivo, "%f %f %f %f \n", i*dx , j*dx , k*dt , pres[i-1][j]);
 				//cout <<  i*dx << " " << j*dx <<" " << k*dt <<" " << pres[i][j] << endl;
