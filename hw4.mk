397eacba5dee8925115612187c3ce1779efedccc
Diegho Argüello <42391122+dfmartinez11@users.noreply.github.com>
2018-11-18 06:40:30 -0500

    Add files via upload

--- hw4.mk
+++ hw4.mk
@@ -1,14 +1,20 @@
-#show: 
 
-	#display MagnitudVelocidad.pdf
-difucion3D.pdf: prubashw4.py difusion.dat
+hw4.pdf: hw4.tex Tpromedios.pdf 
+	
+	pdflatex hw4.tex  
+
+Tpromedios.pdf: difusion3D.pdf
+
+difusion3D.pdf: prubashw4.py difusion.dat
 
 	python prubashw4.py
 
-difusion.dat: comp2 PDE.cpp
+difusion.dat: comp2 PDE.cpp trayectoria.pdf
 
 	g++ PDE.cpp -o comp2
 	./comp2
+
+trayectoria.pdf: MagnitudVelocidad.pdf
 
 MagnitudVelocidad.pdf: Plots_hw4.py MagnitudVelocidad.dat 
 
@@ -18,6 +24,10 @@ MagnitudVelocidad.dat: comp ODE.cpp
 
 	g++ ODE.cpp -o comp
 	./comp
+
+
+
+
 
 
 
